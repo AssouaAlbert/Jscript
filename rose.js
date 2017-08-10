602601fdc6b4a73524a0a2dfc129cf36197d46dd
Assoua Albert <assouaalbert@gmail.com>
2017-08-10 14:44:39 +0100

    added the demo8 tag in the htmlfile to display the properties of the object person, the code is in the rose.js file

--- rose.js
+++ rose.js
@@ -3,6 +3,6 @@ function add1()
   {  
               document.getElementById("demo7").innerHTML = " demo7 (5 + 6) = " + (5+6);
   }
-        </* This line of code adds a single object called person and it's properties arefirstname, last name , age and eyecolor */
-var person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
-document.getElementById("demo").innerHTML = person.firstName + " is " + person.age + " years old.";
+        /* This line of code adds a single object called person and it's properties are firstname, last name , age and eyecolor */
+var person = {firstName: "John", lastName:"Doe", age:50, eyeColor:"blue"};
+document.getElementById("demo8").innerHTML = person.firstName + " is " + person.age + " years old.";
