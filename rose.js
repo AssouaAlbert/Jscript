fcc2f72d2acba9e96d3d89f188f99ff37afd388a
Assoua Albert <assouaalbert@gmail.com>
2017-08-10 14:20:47 +0100

    added a SINGLE object called person with four properies will be displayed in demo8 tag

--- rose.js
+++ rose.js
@@ -2,4 +2,6 @@ document.getElementById("demo6").innerHTML = 5 + 6;
 function add1()
   {  
               document.getElementById("demo7").innerHTML = " demo7 (5 + 6) = " + (5+6);
-    }
+  }
+        </* This line of code adds a single object called person and it's properties arefirstname, last name , age and eyecolor */
+var person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
